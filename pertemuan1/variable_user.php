dba726a058657278eeb48aec02804d2c8aeddc71
alfianasuci <91894615+alfianasuci@users.noreply.github.com>
2022-02-26 12:38:35 +0700

    Add files via upload

---
+++ pertemuan1/variable_user.php
@@ -0,0 +1,14 @@
+<?php
+//definisikanvariable
+$nama = "Alfiana Suci";
+$umur = 19;
+$berat = 53;
+
+echo "Nama : $nama ";
+echo "<br/>Umur : $umur Tahun";
+echo "<br?>Berat : $berat Kg";
+
+echo "<br/> Hello $nama Apa kabar";
+echo "<br/> Hai Nama saya $nama umur saya $umur";
+echo "<hr>"
+?>
