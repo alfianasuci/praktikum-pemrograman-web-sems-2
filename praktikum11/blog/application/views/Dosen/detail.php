37be2ae1569b2aab1e8de423d115ddf7dcbe7803
alfianasuci <91894615+alfianasuci@users.noreply.github.com>
2022-06-07 15:20:58 +0700

    praktikum 11

---
+++ praktikum11/blog/application/views/Dosen/detail.php
@@ -0,0 +1,58 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Detail Dosen</title>
+</head>
+<body>
+<div class="col-md-12">
+    <h3>Dosen</h3>
+    <table border="1" class="table">
+        <thead>
+            <tr>
+                <th>Id</th>
+                <th>NIDN</th>
+                <th>Nama</th>
+                <th>Gender</th>
+                <th>Tempat Lahir</th>
+                <th>Tanggal Lahir</th>
+                <th>Pendidikan</th>
+            </tr>
+        </thead>
+        <tbody>
+            <tr>
+                <td><?php echo $guru-> id ?></td>
+                <td><?php echo $guru-> nidn ?></td>
+                <td><?php echo $guru-> nama ?></td>
+                <td><?php echo $guru-> gender ?></td>
+                <td><?php echo $guru-> tmp_lahir ?></td>
+                <td><?php echo $guru-> tgl_lahir ?></td>
+                <td><?php echo $guru-> pendidikan ?></td>
+            </tr>
+        </tbody>
+    </table>
+    <div class="col-md-5 mb-3">
+        <div class="card">
+            <div class="card-body">
+                <div class="d-flex flex-column align-items-center text-center">
+                    <img src="<?= base_url()?>uploads/photos/<?=$guru->id?>.jpg" width="300">
+                    <div class="mt-4">
+                        <h4><?=$guru->nama?></h4>
+                        <p>Foto Dosen</p>
+                        <br/>
+                        <a href="https://www.instagram.com/alfianasuci/"><button class="btn btn-outline-primary">Instagram</button></a>
+                        <?php echo form_open_multipart("dosen/upload");?>
+                        <input type="file" name="foto" size="300"/>
+                        <input type="hidden" name="iddosen" value="<?=$guru->id?>"/>
+                        <br/> <br/>
+                        <input type="submit" value="Upload Foto" class="btn btn-primary"/>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+    </div>
+</body>
+</html>
